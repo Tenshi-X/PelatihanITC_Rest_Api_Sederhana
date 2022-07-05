8e77b05cd3209205afed6cd761c665c4a8110de4
Tenshi-X <sevafarel17@gmail.com>
2022-07-05 10:02:56 +0700

    Menambahkan Project

---
+++ lib/Homepage.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  String? body = '', hasil = '', hasilLain = '';
+  String? info;
+  TextEditingController titleController = TextEditingController();
+
+  @override
+  void initState() {
+    info = 'Silakan Masukkan Judul Film';
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Pencarian Judul Film'),
+          centerTitle: true,
+        ),
+        body: ListView(
+          padding: const EdgeInsets.all(20),
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Title',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: (() async {
+                var response = await http
+                    .get(Uri.parse('http://www.omdbapi.com/?apikey=65159463&s='
+                        '${titleController.text}'));
+                var data = json.decode(response.body) as Map<dynamic, dynamic>;
+                if (response.statusCode == 200 && data['Response'] == 'True') {
+                  info = 'Pencarian berhasil';
+                  setState(() {
+                    body = data['Search'][0]['Title'];
+                    hasilLain = data['Search'][1]['Title'];
+                    hasil = 'Hasil Teratas\t: ${body!}\nHasil Lainnya\t: $hasilLain';
+                  });
+                } else {
+                  info = 'Pencarian gagal';
+                  setState(() {
+                    body = '';
+                  });
+                }
+              }),
+              child: Text('Get'),
+            ),
+            Text(
+              info!,
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Poppins',
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(hasil!),
+          ],
+        ));
+  }
+}
